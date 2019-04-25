bd0f9d1ab06d327b2c02d70e990ee29ce4e11eaa
Ellie <whitefancy@msn.cn>
2019-04-25 20:39:04 +0800

    new code

--- leetcode/string/Main.java
+++ leetcode/string/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String args[]){
-        PATCount so = new PATCount();
+        NumPassword so = new NumPassword();
         so.test();
 
     }
