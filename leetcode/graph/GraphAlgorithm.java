ac871f230b16dca218873ac550729638b0698018
Bu Wenfeng <whitefancy@msn.cn>
2019-04-26 14:43:36 +0800

    new

--- leetcode/graph/GraphAlgorithm.java
+++ leetcode/graph/GraphAlgorithm.java
@@ -1,15 +1,14 @@
 package leetcode.graph;
 
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
-public class GraphTest {
+public class GraphAlgorithm {
 
-    public void testAMG(){
+    public void test(){
         int n =4,e=4;
         Graph graph = new ALGraph(n);
+        //Graph graph = new AMGraph(n);
         for(int i =0;i<n;i++){
             graph.insertVertex("V"+i);
         }
@@ -19,7 +18,8 @@ public class GraphTest {
         graph.insertEdge(2,3,2);
         System.out.println("结点个数是："+graph.getNumOfVertex());
         System.out.println("边的个数是："+graph.getNumOfEdges());
-        DFS1(graph);
+        List<TreeNode> list  = dFSForest(graph);
+        //traverseBFS(graph);
         graph.deleteEdge(1,3);
         System.out.println("删除<V1,V2>边后...");
         System.out.println("结点个数是："+graph.getNumOfVertex());
@@ -27,7 +27,7 @@ public class GraphTest {
     }
     boolean visited[];
     public  void DFS1(Graph G){
-
+// 复杂度 邻接表 O(n+e) 邻接矩阵O(n^2)
         visited = new boolean[G.getNumOfVertex()];
         for(int v = 0;v<G.getNumOfVertex();v++){
             if(!visited[v]) recursiveDFS(G,v);
@@ -53,17 +53,63 @@ public class GraphTest {
                     int cur = (int)stack.peek();
                     boolean hasNeighbor = false;
                     for(int w = G.getFirstNeighbor(cur);w!=-1;w =G.getNextNeighbor(cur,w)) {
-                        if (visited[w] == false) {
+                        if (!visited[w]) {
                             hasNeighbor = true;
                             visited[w] = true;
                             System.out.println(G.getValueByIndex(w));
                             stack.push(w);
                         }
                     }
-                    if(hasNeighbor==false){
+                    if(!hasNeighbor){
                             stack.pop();
                     }
                 }
+            }
+        }
+    }
+    public void traverseBFS(Graph G){
+        // 复杂度 邻接表 O(n+e) 邻接矩阵O(n^2)
+        Queue queue = new ArrayDeque();
+        visited = new boolean[G.getNumOfVertex()];
+        for(int v = 0;v<G.getNumOfVertex();v++){
+            if(!visited[v]){
+                visited[v]=true;
+                System.out.println(G.getValueByIndex(v));
+                queue.add(v);
+                while (!queue.isEmpty()){
+                    int cur = (int)queue.poll();
+                    for(int w = G.getFirstNeighbor(cur);w!=-1;w =G.getNextNeighbor(cur,w)) {
+                        if (!visited[w]) {
+                            visited[w] = true;
+                            System.out.println(G.getValueByIndex(w));
+                            queue.add(w);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public List<TreeNode> dFSForest(Graph G){
+        List<TreeNode> forest = new ArrayList<>();
+        visited = new boolean[G.getNumOfVertex()];
+        for(int v = 0;v<G.getNumOfVertex();v++){
+            if(!visited[v]) {
+                TreeNode root = new TreeNode(G.getValueByIndex(v));
+                forest.add(root);
+                dFSTree(G,v,root);
+            }
+        }
+        return forest;
+    }
+
+    private void dFSTree(Graph G, int v, TreeNode tree) {
+        visited[v]=true;
+        for(int w =G.getFirstNeighbor(v);w>=0;w=G.getNextNeighbor(v,w) ){
+            if(!visited[w]){
+                TreeNode child = new TreeNode(G.getValueByIndex(w));
+                tree.addChild(child);
+                dFSTree( G,  w,child);
             }
         }
     }
--- leetcode/graph/Main.java
+++ leetcode/graph/Main.java
@@ -2,8 +2,8 @@ package leetcode.graph;
 
 public class Main {
     public static void main(String args[]){
-        GraphTest so = new GraphTest();
-        so.testAMG();
+        GraphAlgorithm so = new GraphAlgorithm();
+        so.test();
 
     }
 }
